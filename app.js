4f05c425402e128fb1bee0b600d51ec542637936
BabyKurasa <babykuracha8@gmail.com>
2026-06-27 13:13:26 +0530

    Fix: Serve index.html correctly on Render

--- app.js
+++ app.js
@@ -2,25 +2,31 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+// Connect to MySQL
 require("./config/db");
 
 const githubRoutes = require("./routes/githubRoutes");
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Serve Frontend
+app.use(express.static("public"));
 
 // API Routes
 app.use("/api", githubRoutes);
 
 // Home Route
 app.get("/", (req, res) => {
-    res.send("🚀 GitHub Profile Analyzer API is Running!");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
+// Start Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
